8ef018c622c840ad05b8573054c135565efce53d
hu.po <hugo.p.cmu@gmail.com>
2023-09-12 10:55:00 -0500

    almost quantized

--- mnist.py
+++ mnist.py
@@ -1,16 +1,20 @@
-import random
-
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Subset, TensorDataset
+import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch.quantization import quantize_dynamic
+
 
 EPOCHS = 3
 BATCH_SIZE = 64
 LR=0.001
+QLEVELS = [
+    # torch.float16,
+    torch.qint8,
+]
 
 # Check for GPU availability
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -32,22 +36,55 @@ test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 class SimpleNN(nn.Module):
     def __init__(self):
         super(SimpleNN, self).__init__()
-        self.fc1 = nn.Linear(28 * 28, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 10)
+        self.fc1 = nn.Linear(28 * 28, 32)
+        self.fc2 = nn.Linear(32, 10)
 
     def forward(self, x):
         x = x.view(-1, 28 * 28)
         x = nn.functional.relu(self.fc1(x))
-        x = nn.functional.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
+    
+def model_to_str(model):
+    params_list = []
+    for _, param in model.named_parameters():
+        params_list.append(param.detach().cpu().numpy().flatten())
+    return ",".join(map(str, np.concatenate(params_list)))
+
+def str_to_model(model_str):
+    model = SimpleNN()
+    param_values = [float(i) for i in model_str.split(",")]
+    idx = 0
+    for name, param in model.named_parameters():
+        num_params = np.prod(param.size())
+        new_val = torch.tensor(param_values[idx:idx+num_params]).view_as(param)
+        param.data.copy_(new_val)
+        idx += num_params
+    return model
+
+def compare_models(model_a, model_b):
+    model_a_sd = model_a.state_dict()
+    model_b_sd = model_b.state_dict()
+
+    all_equal = True
+    for key in model_a_sd:
+        if not torch.equal(model_a_sd[key], model_b_sd[key]):
+            all_equal = False
+            print(f"Warning: Mismatch detected for {key}")
+            print("Model A Value:", model_a_sd[key])
+            print("Model B Value:", model_b_sd[key])
+            print("="*50)
+    return all_equal
 
 # Initialize the model, loss function, and optimizer
 model = SimpleNN().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LR)
 writer = SummaryWriter('./logs')
+
+model_str_trajs = {
+    _qlevel : [] for _qlevel in QLEVELS
+}
 
 # Train the model
 for epoch in range(EPOCHS):
@@ -62,6 +99,11 @@ for epoch in range(EPOCHS):
         # Log train loss every batch
         writer.add_scalar('Train loss', loss.item(), i + epoch * len(train_loader))
 
+    # # Model can be saved out as a string
+    # model_str = model_to_str(model)
+    # print(model_str)
+    # assert compare_models(str_to_model(model_str), model)
+
     correct = 0
     total = 0
     with torch.no_grad():
@@ -72,10 +114,33 @@ for epoch in range(EPOCHS):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
     test_accuracy = 100 * correct / total
+    writer.add_scalar('test_acc/raw', test_accuracy, epoch)
 
-    # Log test accuracy every epoch
-    writer.add_scalar('Test accuracy', test_accuracy, epoch)
+    # Quantize the model
+    for qlevel in QLEVELS:
+        quantized_model = quantize_dynamic(model, {torch.nn.Linear}, dtype=qlevel)
+
+        # Log the model string trajectory
+        model_str_trajs[qlevel].append(model_to_str(quantized_model))
+
+        # Test the quantized model
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                output = quantized_model(images)
+                _, predicted = torch.max(output.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        test_accuracy = 100 * correct / total
+        writer.add_scalar(f"test_acc/{qlevel}", test_accuracy, epoch)
 
     print('Epoch: {0:2d} Test accuracy: {1:.2f}%'.format(epoch, test_accuracy))
+
+# Log the model string trajectory
+for qlevel in QLEVELS:
+    for i, model_str in enumerate(model_str_trajs[qlevel]):
+        print(f"Epoch {i} weights: {model_str}")
 
 writer.close()
